275d110bdbdfb76d16e6230f74f356a1a5ebb51e
Suryanshu Nayak <nayaksuryanshu05@gmail.com>
2026-04-13 22:36:22 +0530

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const envResult = dotenv.config({ path: path.join(__dirname, ".env") });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.join(__dirname, ".env") });
+}
 
 const express = require("express");
 const connectDB = require("./config/db");
@@ -78,10 +80,6 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    if (envResult.error) {
-      throw envResult.error;
-    }
-
     const groqModel = process.env.GROQ_MODEL || 'llama-3.3-70b-versatile';
     console.log(`Using Groq model: ${groqModel}`);
 
